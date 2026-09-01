e4c09afe902d25ec42afb079efd68d7618d77503
Jamie Cole <jamiejcole@icloud.com>
2026-09-01 15:18:14 +1000

    added xpi export

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,12 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'extension/newtab',
+    emptyOutDir: true,
+  },
   server: {
     host: '0.0.0.0',
     port: 5173,
